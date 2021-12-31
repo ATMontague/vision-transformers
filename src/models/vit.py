934ce1433ef8870546af941056bb9638d3a7113a
adam <atmontague10@gmail.com>
2021-12-31 13:14:51 -0800

    added attention head

--- src/models/vit.py
+++ src/models/vit.py
@@ -29,30 +29,70 @@ class PatchEmbedding(nn.Module):
         return x
 
 
+class AttentionHead(nn.Module):
+
+    def __init__(self, k_dim, v_dim, q_dim, embed_dim):
+        super(AttentionHead, self).__init__()
+        self.k_dim = k_dim
+        self.v_dim = v_dim
+        self.q_dim = q_dim
+        self.embed_dim = embed_dim
+
+        self.k = nn.Linear(self.embed_dim, self.k_dim)
+        self.v = nn.Linear(self.embed_dim, self.v_dim)
+        self.q = nn.Linear(self.embed_dim, self.q_dim)
+
+        self.softmax = nn.Softmax(dim=2)  # todo: verify which dim to use
+        self.projection = nn.Linear(self.v_dim, self.embed_dim)
+
+    def forward(self, x):
+
+        k = self.k(x)  # [b, embed_size, k_dim] -> [b, 64, 96] 64 is number of patches
+        v = self.v(x)  # [b, embed_size, v_dim] -> [b, 64, 96]
+        q = self.q(x)  # [b, embed_size, q_dim] -> [b, 64, 96]
+
+        print('k: ', k.shape)
+        print('v: ', v.shape)
+        print('q: ', q.shape)
+
+        # multiply keys and queries
+
+        temp = torch.bmm(input=q, mat2=k.permute(0, 2, 1)) / (self.k_dim ** 0.5)  # [b, embed_size, k_dim]
+        print('matmul of keys & queries: ', temp.shape)
+
+        # softmax to norm vals to be between 0 and 1
+        temp = self.softmax(temp)
+
+        # multiply with values to get final result
+        output = torch.bmm(input=temp, mat2=v)
+
+        # ensure that output shape is same as input shape
+        output = self.projection(output)
+
+        # we should have updated vectors here. return...?
+        return output
+
+
 class ViT(nn.Module):
 
-    def __init__(self, input_size, output_size, hidden_dim=128, num_heads=2, dim_feedforward=2048,
-                 dim_k=96, dim_v=96, dim_q=96):
-        """
-        :param input_size: ...
-        :param output_size: ...
-        :param hidden_dim: the dimensionality of the output embeddings used in the final layer
-        :param num_heads: number of transformer heads to use
-        :param dim_feedforward: the dimension of the feedforwardward network
-        :param dim_k: dimension of key vectors
-        :param dim_v: dimension of value vectors
-        :param dim_q: dimension of query vectors
-        """
-        pass
+    def __init__(self, image_size, patch_size, embed_size=768, num_heads=2, dim_feedforward=2048,
+                 k_dim=96, v_dim=96, q_dim=96):
+        super(ViT, self).__init__()
 
-    def embed(self, inputs):
-        """
-        Convert input images into patches and then linear project them so that they can be fed to the transformer.
-        :param inputs: tensor of shape [b, h, w, 3]
-        :return:
-        """
-        pass
+        self.embed = PatchEmbedding(image_size=image_size, patch_size=patch_size, embed_size=embed_size)
+        self.attention = AttentionHead(k_dim=k_dim, v_dim=v_dim, q_dim=q_dim, embed_dim=embed_size)
 
+    def forward(self, x):
+        """
+        :param x: tensor w/ shape [b, 3, h, w]
+        :return: tensor
+        """
 
-if __name__ == '__main__':
-    patch = PatchEmbedding()
+        # patch embed input images
+        x = self.embed(x)  # [b, num_patches, embed_size]
+        print('after embedding: ', x.shape)
+
+        # pass through attention head
+        x = self.attention(x)  # [b, num_patches, embed_size]
+        print('after attention: ', x.shape)
+        return x
